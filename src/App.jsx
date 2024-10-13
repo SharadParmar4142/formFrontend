ac2ab9f4d38f549b62c56bbcaa57414bec6ff9d4
Sharad Parmar <sharadparamr4142@gmail.com>
2024-10-13 17:42:48 +0530

    Some random bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // App.jsx
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import AdminLogin from './pages/adminLogin';
+import AdminLogin from './pages/AdminLogin';
 import UserForm from './pages/UserForm';
 
 function App() {
